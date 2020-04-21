b1c1fde990056e79a28bd739f02f9cfb9ec806f7
Abhinav Talari <49162896+AbhinavTalari@users.noreply.github.com>
2020-04-22 01:07:27 +0530

    added more qn

--- Arrays/inversion.cpp
+++ Arrays/inversion.cpp
@@ -1,9 +1,93 @@
 #include<bits/stdc++.h>
+#define ll long long
+
 
 using namespace std;
+
+ll _mergeSort(ll arr[], ll temp[], ll left, ll right); 
+ll merge(ll arr[], ll temp[], ll left, ll mid, ll right); 
+  
+
+ll mergeSort(ll arr[], ll array_size) 
+{ 
+    ll temp[array_size]; 
+    return _mergeSort(arr, temp, 0, array_size - 1); 
+} 
+  
+
+
+ll _mergeSort(ll arr[], ll temp[], ll left, ll right) 
+{ 
+    ll mid, inv_count = 0; 
+    if (right > left) { 
+      
+        mid = (right + left) / 2; 
+  
+      
+        inv_count += _mergeSort(arr, temp, left, mid); 
+        inv_count += _mergeSort(arr, temp, mid + 1, right); 
+  
+        /*Merge the two parts*/
+        inv_count += merge(arr, temp, left, mid + 1, right); 
+    } 
+    return inv_count; 
+} 
+
+ll merge(ll arr[], ll temp[], ll left, 
+          ll mid, ll right) 
+{ 
+    ll i, j, k; 
+    ll inv_count = 0; 
+  
+    i = left; 
+    j = mid; 
+    k = left; 
+    while ((i <= mid - 1) && (j <= right)) { 
+        if (arr[i] <= arr[j]) { 
+            temp[k++] = arr[i++]; 
+        } 
+        else { 
+            temp[k++] = arr[j++]; 
+  
+            inv_count = inv_count + (mid - i); 
+        } 
+    } 
+
+    while (i <= mid - 1) 
+        temp[k++] = arr[i++]; 
+  
+  
+    while (j <= right) 
+        temp[k++] = arr[j++]; 
+  
+    
+    for (i = left; i <= right; i++) 
+        arr[i] = temp[i]; 
+  
+    return inv_count; 
+} 
+
+
+
 int main()
 {
-    int t;
+    ll t;
     cin>>t;
+    while (t--)
+    {
+        ll n;
+        cin>>n;
+        ll  array[n];
+         ll temp[n];
+      for(ll  i=0;i<n;i++)
+        {
+            cin>>array[i];
+        }
+        ll total_inv=mergeSort(array,n);
+        cout<<total_inv<<endl;
+       
+    }
+
+    
     
 }
--- Arrays/majority.cpp
+++ Arrays/majority.cpp
@@ -10,30 +10,58 @@ int main()
         int n;
         vector<int>array;
         cin>>n;
-        unordered_map<int,int> mp;
-        int fin=0;
-        int length=n;
+      
         while(n--)
         {
             int inp;
             cin>>inp;
             array.push_back(inp);
-            mp[inp]++;
-            if(mp[inp]>length/2)
-                fin=inp;
             
+                
         }
-        if (fin>0)
-        cout<<fin<<endl;
-        else
+        sort(array.begin(),array.end());
+        int element=-1;
+        int max_freq=0;
+        int count=0;
+        int temp=array[0];
+        for(int i=1;i<n;i++)
         {
-            cout<<-1<<endl;
-        }
-        
+            if(temp=array[i])
+                {
+                    count++;
+                    
+                }
 
+            else{
+                count=1;
+                temp=array[i];
+            }
+
+            if(max_freq<count)
+                {
+                    max_freq=count;
+                    element=array[i];
+                    if(max_freq>n/2)
+                        {
+                            cout<<element<<endl;
+                            break;
+                        }
+
+                }
+
+
+        }
+
+        cout<<((element>-1)?element:-1)<<endl;
+            
+
+        }
+       
+
+        
+        
         
 
        
     }
     
-}
